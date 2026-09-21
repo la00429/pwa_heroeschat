c27fa36302087249338001bacfa0e47c0440c5d9
Laura Figueredo <112426346+la00429@users.noreply.github.com>
2026-09-21 21:17:10 +0000

    Add service worker and caching functionality for PWA
    - Implemented dynamic caching in sw-utils.js to update cache with new responses.
    - Created jshintrc configuration file for JavaScript linting.
    - Added manifest.json for PWA configuration including app icons and display settings.
    - Developed sw.js to manage service worker lifecycle, including installation, activation, and fetch events for caching strategies.

---
+++ js/sw-utils.js
@@ -0,0 +1,13 @@
+
+// Guardar  en el cache dinamico
+function actualizaCacheDinamico(dynamicCache,
+    req, res) {
+    if (res.ok || res.type === 'opaque') {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res;
+        });
+    } else {
+        return res;
+    }
+}
